e661dc1521fc50017f3222e78bbe915d511448b3
Reyst <oleksenko.pm@gmail.com>
2020-11-23 15:23:34 +0200

    add handle and settings for view

--- app/src/main/java/com/urancompany/cycles/view/CyclesView.kt
+++ app/src/main/java/com/urancompany/cycles/view/CyclesView.kt
@@ -18,11 +18,6 @@ class CyclesView @JvmOverloads constructor(
     defStyleAttr: Int = 0,
 ) : View(context, attrs, defStyleAttr) {
 
-    init {
-        initValues()
-    }
-
-
     private var currentAngle: Float = 0F
 
     private var baseHeight: Int = 0
@@ -33,34 +28,71 @@ class CyclesView @JvmOverloads constructor(
 
     private var ringWidth: Float = 100F
 
-    private var daysInCycle: Int = 28
-//        set(value) {
-//            field = value
-//            updateAngles()
-//        }
+    private var handleOuterRadius: Float = 150F
+    private var handleInnerRadius: Float = 16F
 
-    private var anglePerDay: Float = 360 / 32F
+    //    var selectedDay: Int = 0
+    //    private set
 
-    private val periodAngles = mutableListOf<Float>()
-
-    private val circleColors = listOf(Color.RED, Color.YELLOW, Color.GREEN, Color.BLUE)
-
-    private val circlePaint = Paint(Paint.ANTI_ALIAS_FLAG)
-        .apply {
-            style = Paint.Style.FILL
-            strokeWidth = 2F
+    var daysInCycle: Int = DEFAULT_CYCLE_LENGTH
+        set(value) {
+            field = value.takeIf { it in LENGTH_RANGE } ?: DEFAULT_CYCLE_LENGTH
+            updateAngles()
+            invalidate()
         }
+
+    private var anglePerDay: Float = 360F / 32
+
+    private var phaseAngles = emptyList<Float>()
+
+    private val circleColors = mutableListOf<Int>()
+
+    private val mainPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        strokeWidth = 1F
+    }
 
     private val circleParts = mutableListOf<Path>()
     private val pathMatrix = Matrix()
 
+    private var handleOuterRound: RectF = RectF()
+    private var handleInnerRound: RectF = RectF()
 
-    private fun initValues() {
+    private var handleOuterColor = Color.BLACK
+    private var handleInnerColor = Color.WHITE
 
+    init {
+        //daysInCycle = 28
+        val attributes = context.theme.obtainStyledAttributes(attrs, R.styleable.CyclesView, 0, 0)
+        with(attributes) {
+            try {
+                ringWidth = getDimension(R.styleable.CyclesView_ring_width, 100F)
+                circleColors.clear()
+                circleColors.add(getColor(R.styleable.CyclesView_color1, Color.RED))
+                circleColors.add(getColor(R.styleable.CyclesView_color2, Color.YELLOW))
+                circleColors.add(getColor(R.styleable.CyclesView_color3, Color.BLUE))
+                circleColors.add(getColor(R.styleable.CyclesView_color4, Color.GREEN))
 
+                handleOuterRadius = getDimension(R.styleable.CyclesView_handle_outer_radius, ringWidth * 0.7F)
+                handleInnerRadius = getDimension(R.styleable.CyclesView_handle_inner_radius, 16F)
 
+                handleOuterColor = getColor(R.styleable.CyclesView_handle_outer_color, Color.BLACK)
+                handleInnerColor = getColor(R.styleable.CyclesView_handle_inner_color, Color.WHITE)
+
+            } finally {
+                recycle()
+            }
+        }
+        updateAngles()
     }
 
+    private fun updateAngles() {
+        val lengthInDays = 1 + (daysInCycle.takeIf { it > 31 } ?: 31)
+        anglePerDay = 360F / lengthInDays
+
+        val phases = obtainPhasesLengthsByDuration(daysInCycle)
+        phaseAngles = phases.toAngles(anglePerDay)
+    }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
@@ -68,11 +100,11 @@ class CyclesView @JvmOverloads constructor(
             baseHeight = h
             baseWidth = w
 
-            recalculateRing()
+            recalculate()
         }
     }
 
-    private fun recalculateRing() {
+    private fun recalculate() {
         circleParts.clear()
 
         val h = baseHeight
@@ -82,13 +114,12 @@ class CyclesView @JvmOverloads constructor(
 
         circleParts.addAll(
             listOf(
-                createRingPart(outRadius, 0F, 80F, startBorder = BorderType.ANGLE_IN),
-                createRingPart(outRadius, 80F, 60F),
-                createRingPart(outRadius, 140F, 80F),
-                createRingPart(outRadius, 220F, 139F, endBorder = BorderType.ANGLE_OUT),
+                createRingPart(outRadius, 0F, phaseAngles[0], startBorder = BorderType.ANGLE_IN),
+                createRingPart(outRadius, phaseAngles[0], phaseAngles[1]),
+                createRingPart(outRadius, phaseAngles[0] + phaseAngles[1], phaseAngles[2]),
+                createRingPart(outRadius, phaseAngles[0] + phaseAngles[1] + phaseAngles[2],  phaseAngles[3], endBorder = BorderType.ANGLE_OUT),
                 //createRingPart(outRadius, -90F, 180F, endBorder = BorderType.ANGLE_IN),
             )
-
         )
 
         val startX = w - h.toFloat()
@@ -96,8 +127,14 @@ class CyclesView @JvmOverloads constructor(
         centerY = h / 2F
 
         pathMatrix.reset()
-        //        pathMatrix.preRotate(ANGLE_OFFSET)
         pathMatrix.postTranslate(centerX, centerY)
+
+        handleOuterRound = getRectForHandleOval(ANGLE_OFFSET, outRadius, handleOuterRadius)
+            .apply { transform(pathMatrix) }
+
+        handleInnerRound = getRectForHandleOval(ANGLE_OFFSET, outRadius, handleInnerRadius)
+            .apply { transform(pathMatrix) }
+
     }
 
     private fun createRingPart(
@@ -142,47 +179,67 @@ class CyclesView @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         canvas?.drawARGB(80, 102, 204, 255) // @oleksenko: remove
+
+        pathMatrix.preRotate(ANGLE_OFFSET + currentAngle)
+
         circleParts
             .forEachIndexed { index, path ->
                 val colorIndex = index % circleColors.size
-                circlePaint.color = circleColors[colorIndex]
+                mainPaint.color = circleColors[colorIndex]
                 path.transform(pathMatrix)
-                canvas?.drawPath(path, circlePaint)
+                canvas?.drawPath(path, mainPaint)
             }
+
+        mainPaint.color = handleOuterColor
+        canvas?.drawOval(handleOuterRound, mainPaint)
+
+        mainPaint.color = handleInnerColor
+        canvas?.drawOval(handleInnerRound, mainPaint)
 
     }
 
     private fun Path.drawRoundInEnd(radius: Float, angle: Float) {
-        val oval = getRoundEndOval(angle, radius)
+        val oval = getRectForOval(angle, radius, ringWidth / 2F)
         arcTo(oval, angle, -180F)
     }
 
     private fun Path.drawRoundOutEnd(radius: Float, angle: Float) {
-        val oval = getRoundEndOval(angle, radius)
+        val oval = getRectForOval(angle, radius, ringWidth / 2F)
         arcTo(oval, angle, 180F)
     }
 
     private fun Path.drawRoundInStart(radius: Float, angle: Float) {
-        val oval = getRoundEndOval(angle, radius)
+        val oval = getRectForOval(angle, radius, ringWidth / 2F)
         arcTo(oval, angle, 180F)
     }
 
     private fun Path.drawRoundOutStart(radius: Float, angle: Float) {
-        val oval = getRoundEndOval(angle, radius)
+        val oval = getRectForOval(angle, radius, ringWidth / 2F)
         arcTo(oval, angle, -180F)
     }
 
-    private fun getRoundEndOval(angle: Float, radius: Float): RectF {
+    private fun getRectForOval(angle: Float, outerRadius: Float, roundRadius: Float): RectF {
         val angleR = Math.toRadians(angle.toDouble())
-
-        val roundRadius = (ringWidth) / 2F
         val src = RectF(-roundRadius, -roundRadius, roundRadius, roundRadius)
+
         val dstMatrix = Matrix()
-        val dx = (radius - roundRadius) * cos(angleR)
-        val dy = (radius - roundRadius) * sin(angleR)
+        val dx = (outerRadius - roundRadius) * cos(angleR)
+        val dy = (outerRadius - roundRadius) * sin(angleR)
         dstMatrix.setTranslate(dx.toFloat(), dy.toFloat())
         return src.transform(dstMatrix)
     }
+
+    private fun getRectForHandleOval(angle: Float, outerRadius: Float, roundRadius: Float): RectF {
+        val angleR = Math.toRadians(angle.toDouble())
+        val src = RectF(-roundRadius, -roundRadius, roundRadius, roundRadius)
+
+        val dstMatrix = Matrix()
+        val dx = (outerRadius - ringWidth / 2F) * cos(angleR)
+        val dy = (outerRadius - ringWidth / 2F) * sin(angleR)
+        dstMatrix.setTranslate(dx.toFloat(), dy.toFloat())
+        return src.transform(dstMatrix)
+    }
+
 
     private fun Path.drawAngleOutEnd(angle: Float) {
 
@@ -245,7 +302,57 @@ class CyclesView @JvmOverloads constructor(
 
     companion object {
         private const val ANGLE_OFFSET = -45F
+        private const val DEFAULT_CYCLE_LENGTH = 28
+        private val LENGTH_RANGE = 20..42
     }
 
+    private data class PhasesLengths(
+        val phase1: Int,
+        val phase2: Int,
+        val phase3: Int,
+        val phase4: Int,
+    )
+
+    private fun PhasesLengths.toAngles(dayAngle : Float) = listOf(
+        phase1 * dayAngle,
+        phase2 * dayAngle,
+        phase3 * dayAngle,
+        phase4 * dayAngle,
+    )
+
+    private fun obtainPhasesLengthsByDuration(duration: Int): PhasesLengths {
+
+        val ph1: Int = when(duration) {
+            20 -> 4
+            in 21..25 -> 5
+            in 26..31 -> 6
+            in 32..36 -> 7
+            else -> 8
+        }
+
+        val ph2: Int = when (duration) {
+            20, 21 -> 7
+            22, 23 -> 8
+            24, 25, 26 -> 9
+            27, 28 -> 10
+            in 29..32 -> 11
+            33, 34 -> 12
+            in 35..38 -> 13
+            39 -> 14
+            else -> 15
+        }
+
+        val ph3: Int = when(duration) {
+            20 -> 3
+            in 21..24 -> 4
+            in 25..30 -> 5
+            in 31..35 -> 6
+            else -> 7
+        }
+
+        val ph4: Int = duration - ph1 - ph2 - ph3
+
+        return PhasesLengths(ph1, ph2, ph3, ph4)
+    }
 }
 
